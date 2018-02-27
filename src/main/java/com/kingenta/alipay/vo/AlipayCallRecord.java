5164d275f77540a2daa608b1c6dc025845796829
huahua-git <jeff_12345678@163.com>
2018-02-27 10:52:36 +0800

    ffff

--- src/main/java/com/kingenta/alipay/vo/AlipayCallRecord.java
+++ src/main/java/com/kingenta/alipay/vo/AlipayCallRecord.java
@@ -13,7 +13,6 @@ public class AlipayCallRecord implements Serializable {
 	private String callErrorMessage;
 	private String callResponse;
 	private String callTime;
-	private String moreAttr;
 	
 	public String getId() {
 		return id;
